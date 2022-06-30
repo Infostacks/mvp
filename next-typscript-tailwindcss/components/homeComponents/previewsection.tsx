37d068aa535092f1830bb768299cdb9c223426a1
Muhammad Asif <asif88383@gmail.com>
2022-07-01 02:04:25 +0500

    preview section animation updated

--- next-typscript-tailwindcss/components/homeComponents/previewsection.tsx
+++ next-typscript-tailwindcss/components/homeComponents/previewsection.tsx
@@ -2,54 +2,60 @@ import { MdExpandMore } from "react-icons/md";
 // import Image from "next/image";
 import { motion } from "framer-motion";
 import React, { useEffect, useRef, useState } from "react";
+import animList from '../utils/data.js'
+import 'animate.css'
+import TrackVisibility from 'react-on-screen'
+import animData from "../utils/data.js";
+// import {isVisible} from '@testing-library/user-event/dist/utils'
 
 const previewsection = () => {
-  const animList = [
-    "https://assets9.lottiefiles.com/packages/lf20_hcwpcdew.json",
-    "https://assets2.lottiefiles.com/packages/lf20_01jwptn4.json",
-    "https://assets4.lottiefiles.com/packages/lf20_mjr11zfo.json",
-    "https://assets4.lottiefiles.com/packages/lf20_pwohahvd.json",
-    "https://assets4.lottiefiles.com/packages/lf20_jtbfg2nb.json",
-    "https://assets8.lottiefiles.com/packages/lf20_s72aydoa.json",
-    "https://assets4.lottiefiles.com/packages/lf20_rhe84poa.json",
-    "https://assets4.lottiefiles.com/packages/lf20_fdo4fhy6.json",
-    "https://assets4.lottiefiles.com/packages/lf20_wqd1jwoz.json",
-    "https://assets4.lottiefiles.com/packages/lf20_8morfz5o/divices2.json",
-    "https://assets4.lottiefiles.com/packages/lf20_9oliqaij.json",
-    "https://assets4.lottiefiles.com/packages/lf20_2lruogho.json",
-    "https://assets4.lottiefiles.com/packages/lf20_xfx6wio6.json",
-    "https://assets4.lottiefiles.com/packages/lf20_6YVCqP.json",
-    "https://assets8.lottiefiles.com/packages/lf20_yjkyGT.json",
-    "https://assets8.lottiefiles.com/private_files/lf30_vAtD7F.json",
-    "https://assets8.lottiefiles.com/private_files/lf30_FQVNNa.json",
-    "https://assets8.lottiefiles.com/private_files/lf30_51QyOY.json",
-  ];
+
+  const animList = animData;
+  const [isVisible, setIsVisible] = useState(false);
 
   const [animListNo, setAnimListNo] = useState(0);
-  const isMounted = useRef(false);
 
   useEffect(() => {
-    console.log("im here");
-    let number = localStorage.getItem("animList");
-    console.log("number 1st: ", number);
-    // number = animListNo + 1 >= animList.length ? "0" : (animListNo + 1).toString();
-    // console.log("number 2nd: ", number);
-    // localStorage.setItem("animList", (Number(number)+1).toString());
-    // setAnimListNo(Number(number));
 
-    if (number === undefined) {
-      setAnimListNo(0);
-      localStorage.setItem("animList", "0");
-    } else {
+    let number = localStorage.getItem("animList");
+
+    // if (number === undefined) {
+    //   setAnimListNo(0);
+    //   localStorage.setItem("animList", "0");
+    // } else {
+    //   number =
+    //   Number(number) + 1 >= animList.length
+    //   ? "0"
+    //   : (Number(number) + 1).toString();
+    //   console.log("number 2nd: ", number);
+    //   localStorage.setItem("animList", number);
+    //   setAnimListNo(Number(number));
+    // }
+
+    function changeNumber() {
       number =
-      Number(number) + 1 >= animList.length
-      ? "0"
-      : (Number(number) + 1).toString();
-      console.log("number 2nd: ", number);
-      localStorage.setItem("animList", number);
-      setAnimListNo(Number(number));
+        Number(number) + 1 >= animList.length
+        ? "0"
+        : (Number(number) + 1).toString();
+        localStorage.setItem("animList", number);
+        setAnimListNo(Number(number));
     }
-  }, []);
+
+    setInterval(changeNumber, 3000);
+
+    // if(isVisible){
+    //   number =
+    //   Number(number) + 1 >= animList.length
+    //   ? "0"
+    //   : (Number(number) + 1).toString();
+    //   localStorage.setItem("animList", number);
+    //   setAnimListNo(Number(number));
+
+    //   setTimeout(() => {
+    //     setIsVisible(false);
+    //   }, 1500);
+    // }
+  }, [animList]);
 
   const ref = useRef(null);
   useEffect(() => {
@@ -64,16 +70,21 @@ const previewsection = () => {
       {/* Left Side  */}
       <div className="flex justify-center w-1/2">
         <div className="flex flex-col justify-center gap-5 w-2/3">
-          <h3 className="text-violet-500 text-lg">WE CREATE IDEAS</h3>
-          <h1 className="text-3xl font-extrabold text-slate-800">
-            Build Stunning <span className="text-violet-500">Website</span>{" "}
-            InfoStack Tech
-          </h1>
-          <p className="text-sm text-slate-600">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet,
-            repellendus nihil fuga unde voluptas voluptates perspiciatis
-            aspernatur.
-          </p>
+          {/* <TrackVisibility> */}
+            {/* <div className={`flex flex-col justify-center w-full
+            ${isVisible? "animated__animated animate__fadeIn": ""}`}> */}
+              <h3 className="text-violet-500 text-lg">WE CREATE IDEAS</h3>
+              <h1 className="text-3xl font-extrabold text-slate-800">
+                Build Stunning <span className="text-violet-500">Website</span>{" "}
+                InfoStack Tech
+              </h1>
+              <p className="text-sm text-slate-600">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Eveniet, repellendus nihil fuga unde voluptas voluptates
+                perspiciatis aspernatur.
+              </p>
+            {/* </div>
+          </TrackVisibility> */}
           <div className="flex flex-row items-center justify-start gap-3">
             <button className="rounded-full bg-violet-500 p-2 text-3xl text-white">
               <motion.div
@@ -117,8 +128,9 @@ const previewsection = () => {
           ref={ref}
           autoplay
           // controls
-          loop
-          // mode="normal"
+          // loop
+          // complete={() => (setIsVisible(true))}
+          mode="normal"
           // src="https://assets2.lottiefiles.com/packages/lf20_01jwptn4.json"
           src={animList[animListNo]}
         ></lottie-player>
