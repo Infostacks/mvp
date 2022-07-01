481c6e3d1ad4d40e0910a985a2ecf6da1917d4ed
Muhammad Asif <asif88383@gmail.com>
2022-07-01 11:12:33 +0500

    removed unecessary libraries

--- next-typscript-tailwindcss/components/homeComponents/previewsection.tsx
+++ next-typscript-tailwindcss/components/homeComponents/previewsection.tsx
@@ -2,21 +2,15 @@ import { MdExpandMore } from "react-icons/md";
 // import Image from "next/image";
 import { motion } from "framer-motion";
 import React, { useEffect, useRef, useState } from "react";
-import animList from '../utils/data.js'
-import 'animate.css'
-import TrackVisibility from 'react-on-screen'
 import animData from "../utils/data.js";
-// import {isVisible} from '@testing-library/user-event/dist/utils'
 
 const previewsection = () => {
-
   const animList = animData;
   const [isVisible, setIsVisible] = useState(false);
 
   const [animListNo, setAnimListNo] = useState(0);
 
   useEffect(() => {
-
     let number = localStorage.getItem("animList");
 
     // if (number === undefined) {
@@ -35,10 +29,10 @@ const previewsection = () => {
     function changeNumber() {
       number =
         Number(number) + 1 >= animList.length
-        ? "0"
-        : (Number(number) + 1).toString();
-        localStorage.setItem("animList", number);
-        setAnimListNo(Number(number));
+          ? "0"
+          : (Number(number) + 1).toString();
+      localStorage.setItem("animList", number);
+      setAnimListNo(Number(number));
     }
 
     setInterval(changeNumber, 3000);
@@ -60,7 +54,7 @@ const previewsection = () => {
   const ref = useRef(null);
   useEffect(() => {
     import("@lottiefiles/lottie-player");
-  },[]);
+  }, []);
 
   return (
     <div
@@ -70,21 +64,16 @@ const previewsection = () => {
       {/* Left Side  */}
       <div className="flex justify-center w-1/2">
         <div className="flex flex-col justify-center gap-5 w-2/3">
-          {/* <TrackVisibility> */}
-            {/* <div className={`flex flex-col justify-center w-full
-            ${isVisible? "animated__animated animate__fadeIn": ""}`}> */}
-              <h3 className="text-violet-500 text-lg">WE CREATE IDEAS</h3>
-              <h1 className="text-3xl font-extrabold text-slate-800">
-                Build Stunning <span className="text-violet-500">Website</span>{" "}
-                InfoStack Tech
-              </h1>
-              <p className="text-sm text-slate-600">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Eveniet, repellendus nihil fuga unde voluptas voluptates
-                perspiciatis aspernatur.
-              </p>
-            {/* </div>
-          </TrackVisibility> */}
+          <h3 className="text-violet-500 text-lg">WE CREATE IDEAS</h3>
+          <h1 className="text-3xl font-extrabold text-slate-800">
+            Build Stunning <span className="text-violet-500">Website</span>{" "}
+            InfoStack Tech
+          </h1>
+          <p className="text-sm text-slate-600">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet,
+            repellendus nihil fuga unde voluptas voluptates perspiciatis
+            aspernatur.
+          </p>
           <div className="flex flex-row items-center justify-start gap-3">
             <button className="rounded-full bg-violet-500 p-2 text-3xl text-white">
               <motion.div
