63205a215e9b43054f87b45ea89478ab623c954b
Muhammad Asif <asif88383@gmail.com>
2022-07-11 22:26:16 +0500

    contact form created

--- next-typscript-tailwindcss/components/homeComponents/contact.tsx
+++ next-typscript-tailwindcss/components/homeComponents/contact.tsx
@@ -57,7 +57,7 @@ const contact = () => {
             className="flex flex-row items-center justify-center h-screen px-20 py-5 gap-5 w-full"
         >
             {/* Left Side  */}
-            <div className="flex justify-center w-1/2">
+            {/* <div className="flex justify-center w-1/2">
                 <div className="flex flex-col justify-center gap-5 w-2/3">
                     <h1 className="rounded-xl px-3 py-3 w-28 bg-violet-100 text-violet-500 font-semibold">
                         Contact Us
@@ -109,6 +109,53 @@ const contact = () => {
                         </span>
                     </div>
                 </div>
+            </div> */}
+
+            {/* Left side form */}
+            <div className="flex flex-1 justify-center relative">
+                <form className="flex flex-col gap-5 items-centre">
+                    <input type="text" name="user_name" className="user" placeholder="Name" />
+                    <input type="email" name="user_email" className="user" placeholder="Email" />
+                    <textarea name="message" className="user" placeholder="Message" />
+                    {/* <input type="submit" value="Send" className="" /> */}
+                    <div className="flex flex-row items-center justify-start gap-3">
+                        <motion.button
+                            animate={btnAnimation}
+                            type="submit"
+                            className="rounded-full bg-violet-500 p-2 text-3xl text-white"
+                        >
+                            <motion.div
+                                variants={{
+                                    hidden: {
+                                        opacity: 1,
+                                    },
+                                    visible: {
+                                        rotateZ: [25, -25],
+                                        transition: {
+                                            rotateZ: {
+                                                yoyo: Infinity,
+                                                duration: 1,
+                                                delay: 1,
+                                                type: "spring",
+                                            },
+                                        },
+                                    },
+                                    removed: {
+                                        opacity: 1,
+                                    },
+                                }}
+                                initial="hidden"
+                                animate="visible"
+                            >
+                                <MdExpandMore />
+                            </motion.div>
+                        </motion.button>
+                        <span className="text-violet-500 font-semibold hover:text-violet-700 hover:cursor-pointer">
+                            Submit Now
+                        </span>
+                    </div>
+                    {/* <span>{done && "Thanks for Contacting me"}</span> */}
+                </form>
             </div>
 
             {/* Right Side  */}
